0836aa0d22075570c41209d1d320f4cbe3752163
mairomuuga <mairo.muuga@khk.ee>
2022-02-02 16:27:15 +0200

    Added API data to project template file

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const fetch = require('node-fetch')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const key = 'a6e7cf1127ed1dde18d06df7b57d7c7b';
+let city = 'Tartu'
+
 app.get('/', function (req, res){
-	res.render('index')
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+	.then(function(resp) {
+	return resp.json()
+	})
+	.then(function(data) {
+		let description = data.weather[0].description
+		let city = data.name
+		let temp = Math.round(parseFloat(data.main.temp)-273,15)
+	res.render('index', {
+		description: description,
+		city: city,
+		temp: temp
+		})
+	})
 })
 
 app.listen(3000)
